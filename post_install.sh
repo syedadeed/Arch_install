d04204cf2c048b45b84daec6a7bc6a8d470cd607
Syed Adeed <syedadeed@protonmail.com>
2024-12-03 18:22:37 +0530

    update

--- post_install.sh
+++ post_install.sh
@@ -47,7 +47,6 @@ font_installation ()
 gui_setup ()
 {
     sudo pacman -Syy --noconfirm hyprland
-    sudo pacman -Syy --noconfirm kitty
     sudo pacman -Syy --noconfirm grim
     sudo pacman -Syy --noconfirm slurp
     sudo pacman -Syy --noconfirm hyprpicker
