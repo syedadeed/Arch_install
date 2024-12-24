876ec09d60e5b64e0075e59881c0348bb687848b
Syed Adeed <syedadeed@protonmail.com>
2024-12-24 19:32:35 +0530

    update

--- post_install.sh
+++ post_install.sh
@@ -66,7 +66,7 @@ cli_tools_installation ()
     sudo pacman -Syy --noconfirm man-db man-pages
     sudo pacman -Syy --noconfirm git github-cli
     sudo pacman -Syy --noconfirm neovim unzip wl-clipboard
-    sudo pacman -Syy --noconfirm npm
+    sudo pacman -Syy --noconfirm npm typescript
 }
 
 driver_installation ()
