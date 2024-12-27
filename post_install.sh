7982df0d661c2810d637d56282de9b8effaeb6d3
Syed Adeed <syedadeed@protonmail.com>
2024-12-27 16:29:08 +0530

    update

--- post_install.sh
+++ post_install.sh
@@ -67,6 +67,8 @@ cli_tools_installation ()
     sudo pacman -Syy --noconfirm git github-cli
     sudo pacman -Syy --noconfirm neovim unzip wl-clipboard
     sudo pacman -Syy --noconfirm npm typescript
+    sudo pacman -Syy --noconfirm rust
+    sudo pacman -Syy --noconfirm go
     sudo pacman -Syy --noconfirm python-virtualenv python-pip tk
     sudo pacman -Syy --noconfirm luarocks
 }
